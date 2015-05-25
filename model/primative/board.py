cbe167d6e06f21e89afa1a0356970680dbe185f1
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2015-05-24 18:59:32 -0700

    pylint doc update
    describe method and class information

--- model/primative/board.py
+++ model/primative/board.py
@@ -1,15 +1,20 @@
+"""Representation of a board with goals"""
 from .shared import Shared
 from .point import Point
 from .goal import Goal
 
 class Board(object):
+    """Encapsulate matrix and goals"""
+
     def __init__(self, key, board, goalList):
+        """matrix and goal list"""
         self._key = key
         self._board = board
         self._width = len(board)
         self._goals = goalList
 
     def __str__(self):
+        """override string output for debug"""
         return "\tkey: {}; width: {}; \r\n\tboard:\r\n\t\t{}\r\n\tgoals:\r\n\t\t{}".format(
             self._key,
             self._width,
@@ -17,41 +22,48 @@ class Board(object):
             "\r\n\t\t".join([str(g) for g in self._goals]))
 
     def _get_height_width(self):
+        """"width getter"""
         return self._width
     width = height = property(_get_height_width, None)
 
     def _get_key(self):
+        """"key getter"""
         return self._key
     key = property(_get_key, None)
 
     def _get_board(self):
+        """"board getter"""
         return self._board
     board = property(_get_board, None)
 
     def board_value(self, point):
+        """"board value getter"""
         return self._board[point.y][point.x]
     def board_update(self, point, value):
+        """"board value setter"""
         self._board[point.y][point.x] = value
 
     def _get_goals(self):
+        """"goal list getter"""
         return self._goals
     goals = property(_get_goals, None)
 
     def normalize(self):
-        #when possible, match every 1 sided wall with its pair
-        #example a W wall should have a matching E wall next door
-        for y in range(0, self._width):
-            for x in range(0, self._width):
-                if x != 0 and self._board[y][x-1] & Shared.E.value == Shared.E.value:
-                    self._board[y][x] |= Shared.W.value
-                if x != self._width - 1 and self._board[y][x+1] & Shared.W.value == Shared.W.value:
-                    self._board[y][x] |= Shared.E.value
-                if y != 0 and self._board[y-1][x] & Shared.S.value == Shared.S.value:
-                    self._board[y][x] |= Shared.N.value
-                if y != self._width - 1 and self._board[y+1][x] & Shared.N.value == Shared.N.value:
-                    self._board[y][x] |= Shared.S.value
+        """when possible, match every 1 sided wall with its pair
+        example a W wall should have a matching E wall next door"""
+        for col in range(0, self._width):
+            for row in range(0, self._width):
+                if row != 0 and self._board[col][row-1] & Shared.E.value:
+                    self._board[col][row] |= Shared.W.value
+                if row != self._width - 1 and self._board[col][row+1] & Shared.W.value:
+                    self._board[col][row] |= Shared.E.value
+                if col != 0 and self._board[col-1][row] & Shared.S.value:
+                    self._board[col][row] |= Shared.N.value
+                if col != self._width - 1 and self._board[col+1][row] & Shared.N.value:
+                    self._board[col][row] |= Shared.S.value
 
     def rotate(self, iterations=1):
+        """'iterations'*90degree rotation of matrix values and goals"""
         if iterations > 3:
             iterations = iterations % 4
         if iterations == 0:
@@ -61,7 +73,7 @@ class Board(object):
         for j, row in enumerate(self._board):
             for k, column in enumerate(row):
                 for direction in Shared.DIRECTIONS:
-                    if column & direction.value == direction.value:
+                    if column & direction.value:
                         self._board[j][k] += direction.rotate.value - direction.value
         self._goals = [Goal(Point(self.width-1-goal.point.y, goal.point.x), goal.robots)
                        for goal
--- model/primative/goal.py
+++ model/primative/goal.py
@@ -1,13 +1,18 @@
+"""Struct for goal point on board"""
 from .point import Point
 
 class Goal(object):
+    """couple a point with list of winning objects"""
+
     def __init__(self, point, robotList):
+        """require valid instances"""
         assert isinstance(point, Point)
         self.point = point
         assert isinstance(robotList, list)
         self.robots = robotList
 
     def __str__(self):
+        """debug output"""
         stringout = ""
         for robot in self.robots:
             stringout += robot.__str__()
